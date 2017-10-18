1f498957c14ceffd987491516f58adeb18743ecb
Marcel Herd <me@marcelherd.com>
2017-10-18 08:42:37 +0200

    Created bot config from vue-cli template

--- Bot-Config/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(4000, () => {
-    console.log('Bot Config is running on port 4000!');
-});
---
+++ Bot-Config/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <sidebar></sidebar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+
+export default {
+  name: 'app',
+  components: {
+    'sidebar': Sidebar
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>

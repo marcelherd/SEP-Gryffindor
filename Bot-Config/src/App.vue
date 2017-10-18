36900ce8165b9234b6ee610d9c8d9e7b7c1468ca
Marcel Herd <me@marcelherd.com>
2017-10-18 09:19:09 +0200

    Added vuex and vue material

--- Bot-Config/src/App.vue
+++ Bot-Config/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <HelloWorld/>
   </div>
 </template>
@@ -16,13 +15,13 @@ export default {
 }
 </script>
 
+<style src="vue-material/dist/vue-material.css"></style>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

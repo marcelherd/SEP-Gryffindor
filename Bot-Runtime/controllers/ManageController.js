20408f1af08b295116c946ffe5a17d3bf96398e8
Marcel Herd <me@marcelherd.com>
2017-11-11 15:04:27 +0100

    Extracted BotService from Controller logic

--- Bot-Runtime/controllers/ManageController.js
+++ Bot-Runtime/controllers/ManageController.js
@@ -1,91 +1,74 @@
-const cache = require('../cache');
+const botService = require('../services/BotService');
 
 exports.findBot = function (req, res, next, id) {
-  const bot = cache.bots.find(item => item.id === parseInt(id, 10));
+  req.bot = botService.findById(parseInt(id, 10));
 
-  req.bot = bot;
-  next();
+  if (req.bot) {
+    next();
+  } else {
+    // TODO: error handling
+    next(new Error('Bot not found'));
+  }
 };
 
 exports.getBots = function (req, res) {
-  res.send(JSON.stringify(cache.bots));
+  const bots = botService.findAll();
+  res.send(JSON.stringify(bots));
 };
 
 exports.postBot = function (req, res) {
+  // TODO: extract validation logic
   if (!req.body.name || !req.body.template) {
     res.status(400).send('Malformed data');
   } else {
-    const newBot = {
-      id: ++cache.currentId,
-      name: req.body.name,
-      template: req.body.template,
-      status: 'NOT_RUNNING',
-    };
+    const { name, template } = req.body;
+    const id = botService.save(name, template);
 
-    cache.bots.push(newBot);
-
-    res.status(201).send('Bot saved');
+    // TODO: send a more useful response message?
+    res.status(201).send(`${id}`);
   }
 };
 
 exports.getBot = function (req, res) {
-  if (req.bot === undefined) {
-    res.status(404).send('Not found');
-  } else {
-    res.send(JSON.stringify(req.bot));
-  }
+  res.send(JSON.stringify(req.bot));
 };
 
 exports.deleteBot = function (req, res) {
-  const index = cache.bots.findIndex(item => item.id === req.bot.id);
+  // TODO: handle internal errors
+  botService.delete(req.bot);
 
-  if (index === -1) {
-    res.status(404).send('Not found');
-  } else {
-    // TODO: stop the bot, if it is running
-    cache.bots.splice(index, 1);
-    res.send('Bot deleted');
-  }
+  // TODO: send a more useful response message?
+  res.send('Bot deleted');
 };
 
 exports.updateBot = function (req, res) {
-  if (req.bot === undefined) {
-    res.status(404).send('Not found');
-  } else {
-    // TODO update all other properties as well
-    req.bot.name = req.body.name;
-    res.send(JSON.stringify(req.bot));
-  }
+  // TODO: handle internal errors
+  botService.update(req.bot, req.body);
+
+  // TODO: send a more useful response message?
+  res.send('Bot updated');
 };
 
 exports.startBot = function (req, res) {
-  if (req.bot === undefined) {
-    res.status(404).send('Not found');
-  } else {
-    // TODO: start the bot
-    req.bot.status = 'RUNNING';
-    res.send(JSON.stringify(req.bot));
-  }
-};
+  // TODO: handle internal errors
+  botService.start(req.bot);
 
-exports.restartBot = function (req, res) {
-  // TODO: maybe this should simply call stop and then start :D
-  if (req.bot === undefined) {
-    res.status(404).send('Not found');
-  } else {
-    // TODO: restart the bot
-    req.bot.status = 'NOT_RUNNING';
-    req.bot.status = 'RUNNING';
-    res.send(JSON.stringify(req.bot));
-  }
+  // TODO: send a more useful response message?
+  res.send('Bot started');
 };
 
 exports.stopBot = function (req, res) {
-  if (req.bot === undefined) {
-    res.status(404).send('Not found');
-  } else {
-    // TODO: stop the bot
-    req.bot.status = 'NOT_RUNNING';
-    res.send(JSON.stringify(req.bot));
-  }
+  // TODO: handle internal errors
+  botService.stop(req.bot);
+
+  // TODO: send a more useful response message?
+  res.send('Bot stopped');
+};
+
+exports.restartBot = function (req, res) {
+  // TODO: handle internal errors
+  botService.restart(req.bot);
+
+  // TODO: send a more useful response message?
+  res.send('Bot restarted');
 };

0df588c131da55c2197f80858bf0e34a934d3f7c
Marcel Herd <me@marcelherd.com>
2017-11-23 10:44:42 +0100

    bt-badge implemented

---
+++ Bot-Config/src/components/core/Badge.vue
@@ -0,0 +1,50 @@
+<template>
+  <md-layout md-flex class="bt-badge" :class="`bt-badge-${type}`">
+    <span>{{ text }}</span>
+  </md-layout>
+</template>
+
+<script>
+export default {
+  name: 'bt-badge',
+  props: ['text', 'type']
+}
+</script>
+
+<style>
+.bt-badge {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10px;
+  margin-right: 10px;
+  border-radius: 50%;
+}
+
+.bt-badge:before {
+  content: "";
+  display: block;
+  padding-top: 100%;
+}
+
+.bt-badge.bt-badge-success {
+  background-color: #FAD232;
+}
+
+.bt-badge.bt-badge-fail {
+  background-color: #F53A25;
+}
+
+.bt-badge.bt-badge-new {
+  background-color: #7FCEB7;
+}
+
+.bt-badge.bt-badge-overall {
+  background-color: #D1D1D1;
+}
+
+.bt-badge span {
+  font-size: 18px;
+  font-weight: 700;
+}
+</style>
--- Bot-Config/src/components/overview/summary/BotStatistics.vue
+++ Bot-Config/src/components/overview/summary/BotStatistics.vue
@@ -1,58 +1,24 @@
 <template>
   <md-layout md-align="center" md-vertical-align="center">
-    <div class="bt-summary-badge bt-badge-success">
-      <span>2</span>
-    </div>
-    <div class="bt-summary-badge bt-badge-fail">
-      <span>2</span>
-    </div>
-    <div class="bt-summary-badge bt-badge-new">
-      <span>2</span>
-    </div>
-    <div class="bt-summary-badge bt-badge-overall">
-      <span>2</span>
-    </div>
+    <bt-badge text="1" type="success" />
+    <bt-badge text="2" type="fail" />
+    <bt-badge text="3" type="new" />
+    <bt-badge text="4" type="overall" />
   </md-layout>
 </template>
 
 <script>
+import Badge from '@/components/core/Badge'
+
 export default {
   name: 'bt-bot-statistics',
-  props: ['bot']
+  props: ['bot'],
+  components: {
+    'bt-badge': Badge
+  }
 }
 </script>
 
 <style>
-.bt-summary-badge {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-left: 10px;
-  margin-right: 10px;
-  width: 48px;
-  height: 48px;
-  border-radius: 48px;
 
-}
-
-.bt-summary-badge.bt-badge-success {
-  background-color: #FAD232;
-}
-
-.bt-summary-badge.bt-badge-fail {
-  background-color: #F53A25;
-}
-
-.bt-summary-badge.bt-badge-new {
-  background-color: #7FCEB7;
-}
-
-.bt-summary-badge.bt-badge-overall {
-  background-color: #D1D1D1;
-}
-
-.bt-summary-badge span {
-  font-size: 18px;
-  font-weight: 700;
-}
 </style>

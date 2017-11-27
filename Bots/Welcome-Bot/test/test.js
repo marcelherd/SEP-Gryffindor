23192651f5f5730e2ec0833cdb40d989aedf6414
Simsan96 <black-simon@hotmail.de>
2017-11-27 11:47:15 +0100

    working bot

--- Bots/Welcome-Bot/test/test.js
+++ Bots/Welcome-Bot/test/test.js
@@ -1,9 +1,8 @@
-let expect = require('chai').expect;
+const expect = require('chai').expect;
 bot = require('../index');
 
 
-
-describe('Construction of the Welcome-Bot', function () {
+describe('Construction of the Welcome-Bot', () => {
 
   it('Check of a pending promise', function () {
     bot.then(function () {

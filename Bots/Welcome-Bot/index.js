0f1c87a898fbc7aff9180f5969d58588ae86ca93
Simon <black-simon@hotmail.de>
2017-11-28 14:03:59 +0100

    merge

--- Bots/Welcome-Bot/index.js
+++ Bots/Welcome-Bot/index.js
@@ -222,6 +222,125 @@ class GreetingBot {
       },
     });
   }
+  const STRUCTURED_CONTENT = {
+  "type": "vertical",
+  "elements": [
+    {
+      "type": "image",
+      "url": "http://cdn.bgr.com/2016/08/iphone-8-concept.jpg?quality=98&strip=all",
+      "tooltip": "image tooltip",
+      "click": {
+        "actions": [
+          {
+            "type": "navigate",
+            "name": "Navigate to store via image",
+            "lo": 23423423,
+            "la": 2423423423
+          }
+        ]
+      }
+    },
+    {
+      "type": "text",
+      "text": "product name (Title)",
+      "tooltip": "text tooltip",
+      "style": {
+        "bold": true,
+        "size": "large"
+      }
+    },
+    {
+      "type": "text",
+      "text": "product name (Title)",
+      "tooltip": "text tooltip"
+    },
+    {
+      "type": "button",
+      "tooltip": "button tooltip",
+      "title": "Add to cart",
+      "click": {
+        "actions": [
+          {
+            "type": "link",
+            "name": "Add to cart",
+            "uri": "https://example.com"
+          }
+        ]
+      }
+    },
+    {
+      "type": "horizontal",
+      "elements": [
+        {
+          "type": "button",
+          "title": "Buy",
+          "tooltip": "Buy this broduct",
+          "click": {
+            "actions": [
+              {
+                "type": "link",
+                "name": "Buy",
+                "uri": "https://example.com"
+              }
+            ]
+          }
+        },
+        {
+          "type": "button",
+          "title": "Find similar",
+          "tooltip": "store is the thing",
+          "click": {
+            "actions": [
+              {
+                "type": "link",
+                "name": "Buy",
+                "uri": "https://search.com"
+              }
+            ]
+          }
+        }
+      ]
+    },
+    {
+      "type": "button",
+      "tooltip": "button tooltip",
+      "title": "Publish text",
+      "click": {
+        "actions": [
+          {
+            "type": "publishText",
+            "text": "my text"
+          }
+        ]
+      }
+    },
+    {
+      "type": "map",
+      "lo": 64.128597,
+      "la": -21.89611,
+      "tooltip": "map tooltip"
+    },
+    {
+      "type": "button",
+      "tooltip": "button tooltip",
+      "title": "Navigate",
+      "click": {
+        "actions": [
+          {
+            "type": "publishText",
+            "text": "my text"
+          },
+          {
+            "type": "navigate",
+            "name": "Navigate to store via image",
+            "lo": 23423423,
+            "la": 2423423423
+          }
+        ]
+      }
+    }
+  ]
+}
   async sendLink(conversationId, message) {
     if (!this.isConnected) return;
     const index = message.indexOf('http');
@@ -230,20 +349,7 @@ class GreetingBot {
       dialogId: conversationId,
       event: {
         type: 'RichContentEvent',
-        content: {
-          type: 'button',
-          tooltip: 'button tooltip',
-          title: 'Add to cart',
-          click: {
-            actions: [
-              {
-                type: 'link',
-                name: 'Add to cart',
-                uri: link,
-              },
-            ],
-          },
-        },
+        content: this.STRUCTURED_CONTENT
       },
     });
   }

625dff185f969a185b5ced978957b733dea9e564
Marcel Herd <me@marcelherd.com>
2017-12-04 09:31:49 +0100

    i18n start

---
+++ Bot-Config/src/i18n/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+
+const messages = {
+  en: {
+    message: {
+      hello: 'hello world'
+    }
+  },
+  ja: {
+    message: {
+      hello: 'こんにちは、世界'
+    }
+  }
+}
+
+// Create VueI18n instance with options
+const i18n = new VueI18n({
+  locale: 'ja',
+  messages
+})
+
+export default i18n

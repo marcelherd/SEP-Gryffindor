a48cd8508667c2583a91c13ad4eac9f6b5ddfba4
Marcel Herd <me@marcelherd.com>
2017-12-05 15:08:41 +0100

    Viel schlimmer geworden

--- Bot-Config/src/components/layout/PageFooter.vue
+++ Bot-Config/src/components/layout/PageFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <md-layout md-flex="100" class="bt-page-footer">
-    <md-layout md-flex="15" md-flex-xsmall="50" md-flex-small="30" md-flex-medium="30" md-flex-large="20" md-align="center" md-vertical-align="center">
+    <md-layout md-flex="15" md-flex-xsmall="50" md-flex-small="15" md-flex-medium="15" md-flex-large="15" md-align="center" md-vertical-align="center">
       <div class="bt-page-footer-icon">
         <img src="/static/product_white.png" />
       </div>
     </md-layout>
-    <md-layout md-flex md-vertical-align="end">
+    <md-layout md-flex md-vertical-align="center">
       <div class="bt-page-footer-contact">
         <p>BOTTERTOAST</p>
         <p>Mannheim, {{ $t('core.country') }} 2017</p>
@@ -34,16 +34,14 @@ export default {
 
 <style>
 .bt-page-footer {
-  height: 160px;
+  height: 100px;
   background-color: #101929;
   color: white;
 }
 
 .bt-page-footer-icon {
-  width: 140px;
-}
-.bt-page-footer-contact {
-  margin-bottom: 40px;
+  margin-top: 10px;
+  width: 100px;
 }
 
 .bt-page-footer-contact p {

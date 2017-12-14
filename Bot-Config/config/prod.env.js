7dc3334290435e07ad6e4778878a68bc4a5d8de6
Marcel Herd <me@marcelherd.com>
2017-12-14 17:41:20 +0100

    Read Bot Config ip from env variable HOST

--- Bot-Config/config/prod.env.js
+++ Bot-Config/config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  HOST: process.env.HOST ? `"${process.env.HOST}"` : '"localhost"'
 }
--- Bot-Config/src/services/config.js
+++ Bot-Config/src/services/config.js
@@ -1,3 +1,3 @@
 export default {
-  server: 'localhost'
+  server: process.env.HOST
 }
